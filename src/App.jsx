1348a68caa4b397fd97cb77bbc9970f32823100b
ms2041 <74692371+ms2041@users.noreply.github.com>
2023-10-11 21:44:10 +1100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <div>
+      <h1 class="title">The Unfathomable Underworld</h1>
+      <p>An Into the Odd Conversion</p>
+    </div>
+  );
+}
+
+export default App;
